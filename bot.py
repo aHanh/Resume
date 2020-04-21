27757c024282bd06a2dab413299958856373dd2d
aHanh <bao.pham@integrify.io>
2020-04-21 14:10:43 +0300

    1

--- bot.py
+++ bot.py
@@ -1,5 +1,4 @@
-
-print('Top of the world')
-
-
-print('I am the King')
+for i in range(0,3):
+    print("Off with his head!")
+for i in range(4,20):
+    print("This is the {0}th time I've complimented you!".format(i))
